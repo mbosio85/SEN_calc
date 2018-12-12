e9c85696b2e035a0914ad41a7f3ddde3d4ff9f87
mbosio85 <mattia.bosio85@gmail.com>
2018-12-12 13:17:43 +0100

    restyle attempt model1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+//output.innerHTML = slider.value;
+
+//slider.oninput = function() {
+//  output.innerHTML = this.value;
+// }
+
  
 // models variants declaration
 // model1 : Prenatal 3 variants
